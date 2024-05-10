665ba12d71539dec06a5bc532f12d7470056f433
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2024-05-10 13:08:01 +0100

    :tada: Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, MantineProvider, createTheme } from "@mantine/core";
+import React from "react";
+import "@mantine/core/styles.css";
 
 function App() {
+  const theme = createTheme({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider theme={theme}>
+      <h1>Hello World</h1>
+      <Button>Ballsacks</Button>
+    </MantineProvider>
   );
 }
 
