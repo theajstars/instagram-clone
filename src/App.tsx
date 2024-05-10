3b96351021279bfb74b0fb95d8874e5df7534c76
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2024-05-10 13:31:38 +0100

    Bootstrapped project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { Button, MantineProvider, createTheme } from "@mantine/core";
-import React from "react";
+import { useEffect } from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { MantineProvider, createTheme } from "@mantine/core";
+
+import Login from "./Pages/Login";
+
 import "@mantine/core/styles.css";
+
+import "./index.scss";
 
 function App() {
   const theme = createTheme({});
+
   return (
     <MantineProvider theme={theme}>
-      <h1>Hello World</h1>
-      <Button>Ballsacks</Button>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </MantineProvider>
   );
 }
---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import "./styles.scss";
+
+export default function Login() {
+  return (
+    <div className="login-container align-center justify-center flex-row">
+      <h1>Hello World</h1>
+      <h1>...</h1>
+    </div>
+  );
+}
